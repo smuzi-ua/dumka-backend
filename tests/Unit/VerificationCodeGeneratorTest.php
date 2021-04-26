cd6b250b899d7d90d16a39cff223875923e72650
Ostap <osbre@protonmail.com>
2021-04-26 14:21:34 +0300

    unit tests

---
+++ tests/Unit/VerificationCodeGeneratorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\VerificationCodeGenerator;
+use PHPUnit\Framework\TestCase;
+
+class VerificationCodeGeneratorTest extends TestCase
+{
+    private VerificationCodeGenerator $codeGenerator;
+
+    protected function setUp(): void
+    {
+        $this->codeGenerator = app(VerificationCodeGenerator::class);
+        parent::setUp();
+    }
+
+    public function test_it_can_return_valid_output()
+    {
+        $output = $this->codeGenerator->generate();
+
+        $this->assertEquals(5, strlen($output));
+    }
+}
