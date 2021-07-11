00acaae5e2362a59d17effa3f4be01fc25b93341
Ostap Bregin <osbre@protonmail.com>
2021-07-11 21:27:00 +0300

    Set documentation base URL

--- config/scribe.php
+++ config/scribe.php
@@ -19,7 +19,7 @@ return [
     /*
      * The base URL displayed in the docs. If this is empty, Scribe will use the value of config('app.url').
      */
-    'base_url' => null,
+    'base_url' => 'https://smuzi-ua.github.io/dumka-api-documentation/',
 
     /*
      * Tell Scribe what routes to generate documentation for.
